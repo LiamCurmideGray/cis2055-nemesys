ee615bc620c6e61aec2d6cdf85012984aea4b8e2
LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>
2021-04-16 15:31:14 +0200

    Pushing Default Asp .net Core Web App MVC

---
+++ cis2055-NemesysProject/cis2055-NemesysProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cis2055_NemesysProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
