e53ea821a4433e394daf217c1bb0328d00c636a3
stefsche <58699420+stefsche@users.noreply.github.com>
2021-05-01 17:08:18 +0200

    scaffolding for pinpoint and report

---
+++ cis2055-NemesysProject/Controllers/ReportsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cis2055_NemesysProject.Data;
+using cis2055_NemesysProject.Models;
+
+namespace cis2055_NemesysProject.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly cis2055nemesysContext _context;
+
+        public ReportsController(cis2055nemesysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reports
+        public async Task<IActionResult> Index()
+        {
+            var cis2055nemesysContext = _context.Reports.Include(r => r.Pinpoint).Include(r => r.User);
+            return View(await cis2055nemesysContext.ToListAsync());
+        }
+
+        // GET: Reports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports
+                .Include(r => r.Pinpoint)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.ReportId == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // GET: Reports/Create
+        public IActionResult Create()
+        {
+            ViewData["PinpointId"] = new SelectList(_context.Pinpoints, "PinpointId", "PinpointId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email");
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReportId,UserId,PinpointId,DateOfReport,DateTimeHazard,Description,Upvotes,Image")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(report);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PinpointId"] = new SelectList(_context.Pinpoints, "PinpointId", "PinpointId", report.PinpointId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", report.UserId);
+            return View(report);
+        }
+
+        // GET: Reports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            ViewData["PinpointId"] = new SelectList(_context.Pinpoints, "PinpointId", "PinpointId", report.PinpointId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", report.UserId);
+            return View(report);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReportId,UserId,PinpointId,DateOfReport,DateTimeHazard,Description,Upvotes,Image")] Report report)
+        {
+            if (id != report.ReportId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportExists(report.ReportId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PinpointId"] = new SelectList(_context.Pinpoints, "PinpointId", "PinpointId", report.PinpointId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Email", report.UserId);
+            return View(report);
+        }
+
+        // GET: Reports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Reports
+                .Include(r => r.Pinpoint)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.ReportId == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+            _context.Reports.Remove(report);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReportExists(int id)
+        {
+            return _context.Reports.Any(e => e.ReportId == id);
+        }
+    }
+}
