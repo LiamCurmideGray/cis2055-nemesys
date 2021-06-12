ca9969cee0276ec93955ad2429a07624ff2bbb1f
LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>
2021-06-13 01:50:17 +0200

    Added Migration to Add_InvestigationDescription

---
+++ cis2055-NemesysProject/Migrations/20210612233722_Update_InvestigationDescription.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cis2055_NemesysProject.Migrations
+{
+    public partial class Update_InvestigationDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Investigations",
+                type: "nvarchar(max)",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Investigations");
+        }
+    }
+}
